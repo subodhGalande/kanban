a35a4657f120e662452032c252c289de46a951db
Subodh Galande <subodh.dsgn@gmail.com>
2025-11-29 12:38:50 +0530

    chore: updated favicon, title and cleared public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -10,6 +11,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "EB-Kanban Board",
+  description: "engineerBabu assignment",
+};
 
 export default function RootLayout({
   children,
